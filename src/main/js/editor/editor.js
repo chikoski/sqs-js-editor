38f30344e77c517e49d6e3a8b20029f12786896d
N. Shimizu <chikoski@gmail.com>
2013-03-30 01:03:02 +0900

    DOM操作の位置を変更

--- src/main/js/editor/editor.js
+++ src/main/js/editor/editor.js
@@ -3,16 +3,16 @@ define(["jquery", "parser", "models", "views"], function($, parser, model, view)
     var Editor = function(sheet, el){
 	this.$el = $(el);
 	this.sheet = sheet;
-	var v = view.ModelViewMapper.viewOf(this.sheet);
-	if(v){
-	    this.sheetView = new v({model: this.sheet});
-	}
     };
     Editor.prototype = {
 	render: function(){
-	    this.$el.empty();
-	    this.sheetView.render();
-	    this.$el.append(this.sheetView.$el);
+	    var klass = view.ModelViewMapper.viewOf(this.sheet);
+	    if(klass){
+		this.sheetView = new klass({model: this.sheet});
+		this.$el.empty();
+		this.sheetView.render();
+		this.$el.append(this.sheetView.$el);
+	    }
 	}
     };
     
